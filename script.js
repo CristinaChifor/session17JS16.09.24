5eddc295d4b17c0b37921bc93531224400092561
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-09-16 21:58:25 +0300

    workshop(WIP)

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 class Quiz {
-    constructor(questions){}
+    constructor(questions){
+        this.questions = questions;
+        this.questionContainer = document.getElementById("question");
+        this.answerButtons = document.getElementById("answer-buttons");
+        this.feedback = document.getElementById("feedback");
+        this.nextButton = document.getElementById("next-button");
+    }
+
+    showQuestion(question) {
+        this.questionContainer.textContent = question.question;
+    }
 }
+
+async function initQuiz() {
+    const fileContent = await fetch("./questions.json");
+    const quizQuestions = await fileContent.json();
+    console.log(quizQuestions);
+    const quiz = new Quiz(quizQuestions);
+
+}
+
+initQuiz();
