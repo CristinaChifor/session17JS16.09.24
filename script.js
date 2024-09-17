c4b76bec8a84fccce491c67f4b1a9eec9a69efbb
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-09-17 21:28:15 +0300

    Display first question

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ class Quiz {
         this.answerButtons = document.getElementById("answer-buttons");
         this.feedback = document.getElementById("feedback");
         this.nextButton = document.getElementById("next-button");
+        this.showQuestion(this.questions[0]);
+        
     }
 
     showQuestion(question) {
         this.questionContainer.textContent = question.question;
+        this.answerButtons.innerHTML = "";
+        for (const answer of question.answers) {
+            // const answerElement = `<li><button class="btn"></button></li>`;
+            const buttonContainer = document.createElement("li");
+            const buttonElement = document.createElement("button");
+            buttonElement.classList.add("btn");
+            buttonElement.textContent = answer.text;
+            this.answerButtons.appendChild(buttonContainer);
+            buttonContainer.appendChild(buttonElement);
+        }
+
     }
 }
 
